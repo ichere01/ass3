f5e733dbfd56391ce88fb9d8dca0f23c08a52746
Ivan Cherem <ivan.cherem@tufts.edu>
2014-11-18 14:37:25 -0500

    setting up connection

--- server.js
+++ server.js
@@ -3,19 +3,42 @@ var express = require('express');
 var app = express();
 
 // Mongo initialization, setting up a connection to a MongoDB  (on Heroku or localhost)
-/*var mongoUri = process.env.MONGOLAB_URI ||
+var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost/whereintheworld'; // comp20 is the name of the database we are using in MongoDB
+  'https://lit-inlet-6760.herokuapp.com/;' 
 var mongo = require('mongodb');
 var db = mongo.Db.connect(mongoUri, function (error, databaseConnection) {
   db = databaseConnection;
-});*/
+});
 
 app.get('/', function (request, response) {
   response.set('Content-Type', 'text/html');
-  response.send('<p>Hey, it works!</p>');
+  response.send('<p>ks!</p>');
 });
 
+app.post('/sendlocation', function(request, response) {
+	var login = request.body.login;
+	var lat = request.body.lat;
+	var lng = request.body.lng;
+
+	var toInsert = {
+		"login": login,
+		"lat": lat,
+		"lng" = lng,
+	};
+	//CHECK FOR MISSING FIELDS (UNDEFINED)
+
+	db.collection('locations', function(er, collection) {
+		var id = collection.insert(toInsert, function(err, saved) {
+			if (err) {
+				response.send(500);
+			}
+			else {
+				response.send(200);
+			}
+	    });
+	});
+});
 
 
 
